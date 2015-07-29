bee226431698fc5dd1132123cf8a3c92a418f1bd
Unto Kuuranne <unto.kuuranne@aalto.fi>
2015-07-29 20:32:27 +0300

    Separates index and visualize, passing data via localStorage

---
+++ html-ui/scripts/visualize.js
@@ -0,0 +1,30 @@
+
+(function(document) {
+    'use strict';
+
+    var app = document.querySelector('#app');
+
+    /* Defaults for bindings */
+
+    // Listen for template bound event to know when bindings
+    // have resolved and content has been stamped to the page
+    app.addEventListener('dom-change', function() {
+        //if(!app.qtConnected) {
+        //    app.$.ajaxDisconnectedModeEnabledVisualizers.generateRequest();
+        //}
+    });
+
+    // See https://github.com/Polymer/polymer/issues/1381
+    window.addEventListener('WebComponentsReady', function() {
+        // imports are loaded and elements have been registered
+//        console.log('current params:', app.params);
+        var stored = JSON.parse(window.localStorage[app.params.visualizer]));
+        // TODO: just load all to app? by Object.keys(stored) app.key = ...
+        app.visualizer = app.params.visualizer;
+        app.visualizationType = stored.visualizationType;
+        app.settings = stored.settings;
+        app.inputFiles = stored.inputFiles;
+    });
+
+})(document);
+
