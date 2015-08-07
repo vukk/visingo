4a1f9e4e2c7aa443fdefd8bbfbbece01daadcfe6
Unto Kuuranne <unto.kuuranne@aalto.fi>
2015-08-07 21:49:12 +0300

    Makes initial fixes for Firefox, toolbar control styles for test-vis-graph are still bugged.

--- html-ui/scripts/visualize.js
+++ html-ui/scripts/visualize.js
@@ -16,15 +16,17 @@
 
     // See https://github.com/Polymer/polymer/issues/1381
     window.addEventListener('WebComponentsReady', function() {
-        // imports are loaded and elements have been registered
-//        console.log('current params:', app.params);
-        var stored = JSON.parse(window.localStorage[app.params.visualizer]);
-        // TODO: just load all to app? by Object.keys(stored) app.key = ...
-        app.visualizer = app.params.visualizer;
-        app.visualizationType = stored.visualizationType;
-        app.settings = stored.settings;
-        app.inputFiles = stored.inputFiles;
+        // routes are also in WebComponentsReady, Firefox seems to care about this
+        Polymer.Base.async(function() {
+            // imports are loaded and elements have been registered
+            //console.log('current params:', app.params);
+            var stored = JSON.parse(window.localStorage[app.params.visualizer]);
+            // TODO: just load all to app? by Object.keys(stored) app.key = ...
+            app.visualizer = app.params.visualizer;
+            app.visualizationType = stored.visualizationType;
+            app.settings = stored.settings;
+            app.inputFiles = stored.inputFiles;
+        },1);
     });
 
 })(document);
-
