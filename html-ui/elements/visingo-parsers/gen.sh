c459cfeefb346a1068b79db961047017c583523f
Unto Kuuranne <unto.kuuranne@aalto.fi>
2015-08-14 14:55:33 +0300

    Cleanup. PageJS import and old parsers are not needed. Filedrop already replaced file-chooser.

--- html-ui/elements/visingo-parsers/gen.sh
+++ html-ui/elements/visingo-parsers/gen.sh
@@ -2,5 +2,3 @@
 
 pegjs -e visingo.parsers.answersettermsToVisjs answersetterms-to-visjs.pegjs
 pegjs -e visingo.parsers.solverTxtToJson solver-txt-to-json.pegjs
-pegjs -e visingo.parsers.singleTerm single-term.pegjs
-pegjs -e visingo.parsers.multipleTerm multiple-term.pegjs
